20ca67c43429bb55663a8e5ec1744260b8880ce5
OysteinKoder <oystein.kodehode@gmail.com>
2022-03-03 11:01:43 +0100

    Test commit

---
+++ src/pages/advicePage/adviceStyles.js
@@ -0,0 +1,10 @@
+import Styled from "styled-components";
+
+export const AdviceBox = Styled.div`
+background-color: salmon; 
+border: solid black 3px; 
+width: 30rem;
+margin: auto; 
+padding: 1rem; 
+margin-top:25%;
+`;
