28e4b30e38725da2843abb025aac146127c4ac6d
OysteinKoder <oystein.kodehode@gmail.com>
2022-03-08 13:03:10 +0100

    First commit

--- src/pages/AdvicePage.js
+++ src/pages/AdvicePage.js
@@ -1,50 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { AdviceBox } from "./advicePage/adviceStyles";
-
+import { adviceData } from "./advicePage/adviceConsts";
 
 export const AdvicePage = () => {
-    const [fact, setFact] = useState()
-    const [isLoaded, setIsLoaded] = useState()
-    const [error, setError] = useState()
-
-    const fetchData = () => {
-        fetch("https://api.adviceslip.com/advice")
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    setIsLoaded(true)
-                    setFact(result)
-                },
-                (error) => {
-                    setIsLoaded(true)
-                    setError(error)
-                }
-            )
-    }
-
-    useEffect(() => {
-        fetchData()
-    }, [])
-    if (fact) {
-        console.log(fact)
-        return (
-            <div>
-                <AdviceBox>{fact.slip.advice}</AdviceBox>
-                <button onClick={fetchData}>get advice</button>
-            </div>
-        )
-    }
-    else if (error && isLoaded) {
-        console.log(error)
-        return (
-            <div>
-                <p>Sorry, there has been an error</p>
-            </div>
-        )
-    }
-    else return (
-        <div>
-            <AdviceBox>The page is loading</AdviceBox>
-        </div>
-    )
-}
+  return (
+    <div>
+      <AdviceBox>{adviceData[0].catFacts.catFactOne}</AdviceBox>
+    </div>
+  );
+};
---
+++ src/pages/advicePage/adviceConsts.js
@@ -0,0 +1,17 @@
+export const adviceData = [
+  {
+    catFacts: {
+      catFactOne: "Cats Have Nine Lives",
+      catFactTwo: "Cats dont give a shit",
+      kittenFacts: {
+        kittenFactOne: "kittens are cute",
+        kittenFactTwo: "kittens are fluffy",
+        mineFacts: {
+          mineFactOne: "you can dig too deep in the mines of moria",
+          mineFactTwo: "beware of the ballrog",
+        },
+      },
+    },
+  },
+  ["hey", "what", "is", "up"],
+];
