979adf638204d497d6bfd23c071d004810739cbc
OysteinKoder <oystein.kodehode@gmail.com>
2022-03-15 09:51:28 +0100

    change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import { AdvicePage } from './pages/AdvicePage';
+import "./App.css";
+import { AdvicePage } from "./pages/AdvicePage";
 
 function App() {
   return (
     <div className="App">
-      <AdvicePage></AdvicePage>
+      <AdvicePage />
     </div>
   );
 }
